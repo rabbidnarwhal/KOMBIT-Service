173063a4e7840b1dfa630ce521614b604561089f
~diva <diva.adwitya@gmail.com>
2018-06-20 01:46:35 +0800

    Add controller to fetch config params

---
+++ KombitServer/Controllers/SysParamController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KombitServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KombitServer.Controllers
+{
+  [Route ("api/config")]
+  public class SysParamController : Controller
+  {
+    private readonly KombitDBContext _context;
+    public SysParamController (KombitDBContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<SysParam> GetAll ()
+    {
+      var config = _context.SysParam.ToList ();
+      return config;
+    }
+
+    [HttpGet ("{code}")]
+    public IActionResult GetParamsByCode (string code)
+    {
+      if (code == null)
+      {
+        return Ok (GetAll ());
+      }
+      var config = _context.SysParam
+        .FirstOrDefault (x => x.ParamCode == code);
+      return Ok (config);
+    }
+  }
+}
