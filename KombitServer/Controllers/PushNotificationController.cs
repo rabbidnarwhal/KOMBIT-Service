5107a2ffb3e9875d67b430642e4b905996f56a10
~diva <diva.adwitya@gmail.com>
2018-06-25 22:46:29 +0800

    Add PushNotification Controller

---
+++ KombitServer/Controllers/PushNotificationController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using KombitServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace KombitServer.Controllers
+{
+  [Route ("api/push")]
+  public class PushNotificationController : Controller
+  {
+    [HttpPost ("topics/{topic}")]
+    public IActionResult PushToTopics ([FromBody] PushNotification push, string topic)
+    {
+      PushNotificationRequestToTopic body = PushNotificationRequestToTopic.init (push, topic);
+      string jsonBody = JsonConvert.SerializeObject (body);
+      return Ok (Push (jsonBody));
+    }
+
+    [HttpPost]
+    public IActionResult PushToUser ([FromBody] PushNotification push)
+    {
+      try
+      {
+        if (push.To == null || push.To.Count == 0)
+        {
+          return BadRequest ();
+        }
+
+      }
+      catch
+      {
+        return BadRequest ();
+      }
+      PushNotificationRequestToUser body = PushNotificationRequestToUser.From (push);
+      string jsonBody = JsonConvert.SerializeObject (body);
+
+      return Ok (Push (jsonBody));
+
+    }
+
+    private string Push (string jsonBody)
+    {
+      string key = "AIzaSyAab6I-ig-VWIEABDcKaVDxJNE-W723uWo";
+      string url = "https://fcm.googleapis.com/fcm/send";
+
+      RestClient client = new RestClient (url);
+      RestRequest request = new RestRequest ();
+
+      request.Method = Method.POST;
+      request.AddHeader ("Content-Type", "application/json");
+      request.AddHeader ("Authorization", "key=" + key);
+      request.AddParameter ("application/json", jsonBody, ParameterType.RequestBody);
+      IRestResponse response = client.Execute (request);
+      return response.Content;
+    }
+
+  }
+
+}
---
+++ KombitServer/Models/PushNotification.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KombitServer.Models
+{
+  public class PushNotification
+  {
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public List<string> To { get; set; }
+  }
+  public class PushNotificationRequestToTopic
+  {
+    public string to { get; set; }
+    public string priority { get; set; }
+    public PushNotificationRequestData data { get; set; }
+
+    public static PushNotificationRequestToTopic init (PushNotification push, string topic)
+    {
+      PushNotificationRequestData data = new PushNotificationRequestData ()
+      {
+        title = push.Title,
+        body = push.Body,
+        style = "inbox",
+        summaryText = "There are %n% notifications"
+      };
+
+      PushNotificationRequestToTopic request = new PushNotificationRequestToTopic ()
+      {
+        to = "/topics/" + topic,
+        priority = "normal",
+        data = data
+      };
+      return request;
+    }
+  }
+  public class PushNotificationRequestToUser
+  {
+    public string[] to { get; set; }
+    public string priority { get; set; }
+    public PushNotificationRequestData data { get; set; }
+
+    public static PushNotificationRequestToUser From (PushNotification push)
+    {
+      PushNotificationRequestData data = new PushNotificationRequestData ()
+      {
+        title = push.Title,
+        body = push.Body,
+        style = "inbox",
+        summaryText = "There are %n% notifications"
+      };
+
+      PushNotificationRequestToUser request = new PushNotificationRequestToUser ()
+      {
+        to = push.To.ToArray (),
+        priority = "normal",
+        data = data
+      };
+      return request;
+    }
+  }
+  public class PushNotificationRequestData
+  {
+    public string title { get; set; }
+    public string body { get; set; }
+    public string style { get; set; }
+    public string summaryText { get; set; }
+  }
+
+}
