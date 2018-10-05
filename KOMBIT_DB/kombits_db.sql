1a8bda99d16a0dc141978bdf5608dc2627f222f2
diva~ <diva.adwitya@gmail.com>
2018-10-05 15:06:15 +0800

    Update db sql

---
+++ KOMBIT_DB/kombits_db.sql
@@ -0,0 +1,165 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.5.5-10.1.16-MariaDB : Database - kombits_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`kombits_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `kombits_db`;
+
+/*Table structure for table `foto_upload` */
+
+DROP TABLE IF EXISTS `foto_upload`;
+
+CREATE TABLE `foto_upload` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL COMMENT 'foto yang diupload  per product_id',
+  `foto_name` varchar(50) NOT NULL,
+  `foto_path` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `foto_upload` */
+
+/*Table structure for table `interaction` */
+
+DROP TABLE IF EXISTS `interaction`;
+
+CREATE TABLE `interaction` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL COMMENT 'product yang di like/comment/view refer table product',
+  `is_like` tinyint(1) DEFAULT NULL,
+  `liked_by` int(11) DEFAULT NULL COMMENT 'refer ke field id table m_user',
+  `liked_date` datetime DEFAULT NULL,
+  `is_comment` tinyint(1) DEFAULT NULL,
+  `comment_by` int(11) DEFAULT NULL COMMENT 'refer ke field id table m_user',
+  `comment_date` datetime DEFAULT NULL,
+  `is_chat` tinyint(1) DEFAULT NULL,
+  `chat_by` int(11) DEFAULT NULL COMMENT 'refer ke field id table m_user',
+  `chat_date` datetime DEFAULT NULL,
+  `is_viewed` tinyint(1) DEFAULT NULL,
+  `viewed_by` int(11) DEFAULT NULL COMMENT 'refer ke field id table m_user',
+  `viewed_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `interaction` */
+
+/*Table structure for table `m_category` */
+
+DROP TABLE IF EXISTS `m_category`;
+
+CREATE TABLE `m_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_category` */
+
+insert  into `m_category`(`id`,`category`) values (1,'IT Solution'),(2,'Humman Resource'),(3,'Network'),(4,'Design'),(5,'Mobile Application'),(6,'CRM'),(7,'Healthcare'),(8,'Fintech'),(9,'Security'),(10,'Logistic');
+
+/*Table structure for table `m_company` */
+
+DROP TABLE IF EXISTS `m_company`;
+
+CREATE TABLE `m_company` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `holding_id` int(11) NOT NULL COMMENT 'refer m_holding',
+  `company_name` varchar(100) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `address_koordinat` varchar(255) NOT NULL COMMENT 'koordinat google map',
+  `fixed_call` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_company` */
+
+insert  into `m_company`(`id`,`holding_id`,`company_name`,`address`,`address_koordinat`,`fixed_call`) values (1,1,'PT.Sigma Cipta Caraka','JL.kaki untuk maju','https://goo.gl/maps/ZffhwyEpVzA2','+62 21 789987');
+
+/*Table structure for table `m_holding` */
+
+DROP TABLE IF EXISTS `m_holding`;
+
+CREATE TABLE `m_holding` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `holding_name` varchar(100) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `address_koordinat` varchar(255) NOT NULL COMMENT 'koordinat google map',
+  `fixed_call` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_holding` */
+
+insert  into `m_holding`(`id`,`holding_name`,`address`,`address_koordinat`,`fixed_call`) values (1,'PT. Telekomunikasi Indonesia','Telkom Landmark Tower, Lantai 39 Jl. Jendral Gatot Subroto Kav. 52 RT.6/RW.1, Kuningan Barat, Mampang Prapatan RT.6/RW.1, Kuningan Barat, Mampang Prapatan Jakarta Selatan, DKI Jakarta, 12710 Indonesia','https://goo.gl/maps/ZffhwyEpVzA2','+62 21 - 5215328');
+
+/*Table structure for table `m_type_id` */
+
+DROP TABLE IF EXISTS `m_type_id`;
+
+CREATE TABLE `m_type_id` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `desc_type` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_type_id` */
+
+insert  into `m_type_id`(`id`,`desc_type`) values (1,'KTP'),(2,'KITAS'),(3,'KIPEM'),(4,'Passport'),(5,'ID Company');
+
+/*Table structure for table `m_user` */
+
+DROP TABLE IF EXISTS `m_user`;
+
+CREATE TABLE `m_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `id_number` varchar(50) NOT NULL,
+  `id_type` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `occupation` varchar(100) NOT NULL,
+  `handphone` varchar(15) NOT NULL,
+  `job_title` varchar(100) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_user` */
+
+/*Table structure for table `product` */
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_id` int(11) NOT NULL COMMENT 'refer m_company',
+  `holding_id` int(11) NOT NULL COMMENT 'refer m_holding',
+  `product_name` varchar(200) NOT NULL,
+  `description` varchar(255) NOT NULL COMMENT 'High Level dan Low level detail',
+  `is_include_price` tinyint(1) NOT NULL COMMENT 'jika ya => memunculkan harga,tidak => harga hide',
+  `price` double DEFAULT NULL,
+  `credentials` varchar(255) DEFAULT NULL COMMENT 'success stories, client name',
+  `video_path` varchar(255) DEFAULT NULL COMMENT 'video about product or testimoni',
+  `user_id` int(11) NOT NULL COMMENT 'refer m_user (kebutuhan contact person)',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `product` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ KOMBIT_DB/kombits_db_update.sql
@@ -0,0 +1,92 @@
+ALTER TABLE `product`
+	ADD COLUMN `category_id` INT(11) NOT NULL COMMENT 'refer m_category' AFTER `holding_id`,
+	ADD COLUMN `currency` VARCHAR(3) NOT NULL AFTER `price`;
+
+ALTER TABLE `interaction`
+	ADD COLUMN `comment` VARCHAR(255) NULL DEFAULT NULL AFTER `liked_date`;
+
+ALTER TABLE `m_company`
+	ADD COLUMN `image` VARCHAR(255) NULL DEFAULT NULL AFTER `fixed_call`;
+
+ALTER TABLE `m_user`
+	ADD COLUMN `address_koordinat` VARCHAR(255) NULL DEFAULT NULL AFTER `address`,
+	ADD COLUMN `image` VARCHAR(255) NULL DEFAULT NULL AFTER `company_id`;
+
+CREATE TABLE `sys_param` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`param_code` VARCHAR(100) NOT NULL COMMENT 'kode param',
+	`param_value` VARCHAR(255) NOT NULL COMMENT 'nilai param yang akan digunakan',
+	`description` VARCHAR(255) NOT NULL COMMENT 'penjelasan fungsi parameter',
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `notification` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`content` VARCHAR(255) NOT NULL,
+	`title` VARCHAR(100) NOT NULL,
+	`topic` VARCHAR(100) NULL DEFAULT NULL COMMENT 'target topic yang akan dikirimkan notification',
+	`to` INT(11) NULL DEFAULT NULL COMMENT 'target user yang akan dikirimkan notification',
+	`push_date` DATETIME NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `m_user`
+	ADD COLUMN `push_id` VARCHAR(255) NULL DEFAULT NULL AFTER `password`;
+	-- ADD COLUMN `jwt_token` VARCHAR(255) NULL DEFAULT NULL AFTER `push_token`;
+
+ALTER TABLE `m_user`
+	ADD COLUMN `provinsi_id` INT(11) NULL DEFAULT NULL AFTER `address`,
+	ADD COLUMN `kab_kota_id` INT(11) NULL DEFAULT NULL AFTER `address`;
+
+CREATE TABLE `m_kab_kota` (
+	`kab_kota_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`provinsi_id` INT(11) NOT NULL,
+	`kab_kota_name` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `m_provinsi` (
+	`provinsi_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`provinsi_name` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `m_user`
+	ADD COLUMN `id_role` INT(2) NOT NULL AFTER `id_type`;
+
+ALTER TABLE `m_user`
+	CHANGE COLUMN `id_number` `id_number` VARCHAR(50) NULL DEFAULT NULL AFTER `push_id`,
+	CHANGE COLUMN `id_type` `id_type` INT(11) NULL DEFAULT NULL AFTER `id_number`,
+	CHANGE COLUMN `name` `name` VARCHAR(100) NULL DEFAULT NULL AFTER `id_role`,
+	CHANGE COLUMN `address` `address` VARCHAR(255) NULL DEFAULT NULL AFTER `email`,
+	CHANGE COLUMN `handphone` `handphone` VARCHAR(15) NULL DEFAULT NULL AFTER `occupation`,
+	CHANGE COLUMN `company_id` `company_id` INT(11) NULL DEFAULT NULL AFTER `job_title`;
+
+ALTER TABLE `product`
+	CHANGE COLUMN `description` `description` TEXT NULL DEFAULT NULL COMMENT 'High Level dan Low level detail' AFTER `product_name`,
+	CHANGE COLUMN `credentials` `credentials` TEXT NULL DEFAULT NULL COMMENT 'success stories, client name' AFTER `price`,
+	CHANGE COLUMN `video_path` `video_path` TEXT NULL DEFAULT NULL COMMENT 'video about product or testimoni' AFTER `credentials`;
+
+ALTER TABLE `foto_upload`
+	ADD COLUMN `use_case` VARCHAR(50) NOT NULL COMMENT 'case foto yang diupload' AFTER `foto_path`;
+
+CREATE TABLE `attachment_file` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`product_id` INT(11) NOT NULL,
+	`file_name` VARCHAR(50) NOT NULL,
+	`file_path` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
+)
+COMMENT='File yang diupload per product'
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `product`
+	ADD COLUMN `business_target` TEXT NULL AFTER `description`,
+	ADD COLUMN `feature` TEXT NULL AFTER `business_target`,
+	ADD COLUMN `benefit` TEXT NULL AFTER `feature`,
+	ADD COLUMN `implementation` TEXT NULL AFTER `benefit`,
+	ADD COLUMN `faq` TEXT NULL AFTER `implementation`,
+	ADD COLUMN `is_promoted` TINYINT(1) NOT NULL DEFAULT '0' AFTER `faq`,
+	ADD COLUMN `contact_name` VARCHAR(50) NOT NULL AFTER `user_id`,
+	ADD COLUMN `contact_handphone` VARCHAR(15) NOT NULL AFTER `contact_name`,
+	ADD COLUMN `contact_email` VARCHAR(100) NOT NULL AFTER `contact_handphone`;
