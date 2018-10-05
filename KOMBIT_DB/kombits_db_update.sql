a4cabdde3ffe8abac0e8cae61b1146db967cd205
Komang Subhawa <k.subhawa@btassurance.com>
2018-10-05 16:11:45 +0800

    fix db script

--- KOMBIT_DB/kombits_db_update.sql
+++ KOMBIT_DB/kombits_db_update.sql
@@ -42,13 +42,13 @@ CREATE TABLE `m_kab_kota` (
 	`kab_kota_id` INT(11) NOT NULL AUTO_INCREMENT,
 	`provinsi_id` INT(11) NOT NULL,
 	`kab_kota_name` VARCHAR(255) NOT NULL,
-	PRIMARY KEY (`id`)
+	PRIMARY KEY (`kab_kota_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `m_provinsi` (
 	`provinsi_id` INT(11) NOT NULL AUTO_INCREMENT,
 	`provinsi_name` VARCHAR(255) NOT NULL,
-	PRIMARY KEY (`id`)
+	PRIMARY KEY (`provinsi_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `m_user`
