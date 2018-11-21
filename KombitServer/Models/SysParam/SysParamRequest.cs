3d8268c39bf636f2ecb4c7060f26e6e908c30402
diva~ <diva.adwitya@gmail.com>
2018-11-21 14:21:54 +0800

    Moved to folder on its own & created sysParamRequest class used to change param on db

---
+++ KombitServer/Models/SysParam/SysParamRequest.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace KombitServer.Models
+{
+    public partial class SysParamRequest
+    {
+        public string ParamCode { get; set; }
+        public string ParamValue { get; set; }
+    }
+}
